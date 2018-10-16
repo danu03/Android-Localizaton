16140b90302356bc9fb85de39b014cb2cf6cb4e3
danu03 <danusuhendra03@gmail.com>
2018-10-16 08:29:17 +0700

    first init

---
+++ app/src/main/java/com/danusuhendra/mylocalization/MainActivity.java
@@ -0,0 +1,26 @@
+package com.danusuhendra.mylocalization;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button Lang = (Button) findViewById(R.id.btnLang);
+        Lang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+    }
+}
